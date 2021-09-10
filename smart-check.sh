4efc731488e780b1efcddebde03a4316514be14e
J Greig <jaygreig86@gmail.com>
2021-09-10 15:50:16 +0100

    Update smart-check.sh

--- smart-check.sh
+++ smart-check.sh
@@ -4,11 +4,12 @@
 # Purpose:
 #   simple bash script that parses the output provided by smartctl
 #   to show relevant information about predictive failures
-#   things are wrong.
+#   that we actually care about.
 #
 # Original Author: James Greig
 # Licence: GNU GPL v3
 
+VERSION=06/08/2021
 
 PATH=/usr/local/sbin:$PATH
 export PATH
@@ -22,9 +23,24 @@ key="$1"
 case $key in
     -d|--debug)
     DEBUG=1
-    shift # past argument
-    shift # past value
+    shift
+    shift
     ;;
+    -u|--update)
+    # wget -O /usr/sbin/smart-check.sh https://mirrors.host-it.co.uk/files/smart-check.sh
+    exit 0;
+    ;;
+    -v|--version)
+    echo $VERSION
+    exit 0;
+    ;;
+    -h|--help)
+    printf "\r\n-d debug\r\n-u update\r\n-h help\r\n-v version\r\n"
+    exit 0;
+    ;;
+    *)
+    printf "Unknown Argument \r\n"
+    exit 0;
 esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
@@ -36,23 +52,32 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 
 #disks=($(ls /dev/sd*|egrep '^(\/)dev(\/)sd[a-z]$'))
 
+SERIALS=()
 function smartcheck()
 {
         error=0
-        # Wear on SSDs via SATA
+        # Obtain the serial
 
-        smartoutput=$(smartctl -a $1)
-        serial=`echo "$smartoutput" | egrep "Serial Number:|Serial number:" | awk '{print $3}'`
-        if [ ${DEBUG} ] &&  [ ${serial} ]
+        smartoutput=$(smartctl -v 1,raw48:54 -v 7,raw48:54 -v 195,raw48:54 -a $1)
+        serial=`echo "$smartoutput" | egrep "Serial Number:|Serial number:" | grep -v "\[No" | awk '{print $3}'`
+        if [[ "${SERIALS[@]}" =~ "${serial}" ]] &&  [ ${serial} ]; then
+                # we've seen this serial before from another device
+                return 0;
+        fi
+        if [ ${serial} ]
         then
-                echo -e "---"
-                echo -e "\e[0;40mScanning $1 Serial: ${serial} \e[0m"
+                SERIALS+=(${serial})
+                if [ ${DEBUG} ]
+                then
+                        echo -e "---"
+                        echo -e "\e[0;40mScanning $1 Serial: ${serial} \e[0m"
+                fi
         fi
 
         # SSD Wear Level
 
         #var=`smartctl -a $1 | grep Wear_Leveling | awk '{print $4}' | sed 's/^0\|^00//'`
-        var=`echo "$smartoutput" | grep Wear_Leveling | awk '{print $4}' | sed 's/^0\|^00//'`
+        var=`echo "$smartoutput" | egrep -i "177 Wear_Leveling|231 SSD_Life_Left|^173 Un|233 Media_Wearout_" | awk '{print $4}' | sed 's/^0\|^00//' | head -n 1`
         if [[ ${var#0} -lt 20 ]] && [[ ${var#0} -gt 0 ]]
         then
                 echo -e "\e[0;41m$1 is at ${var#0}% SSD wear\e[0m"
@@ -89,6 +114,18 @@ function smartcheck()
                 echo -e "\e[30;42m$1 has $var Offline Uncorrectable Errors\e[0m"
         fi
 
+        # Early Warning Raw_Read_Error_Rate
+
+        var=`echo "$smartoutput" | egrep -i "1 Raw_Read_Error_Rate" | awk '{print $10}' | sed 's/^0\|^00//'`
+        if [[ ${var#0} -gt 10 ]] && [[ ${var#0} -gt 0 ]]
+        then
+                echo -e "\e[0;41m$1 has a Read Error Rate of ${var#0}\e[0m"
+                error=$(($error + 1))
+        elif [ ${DEBUG} ] && [[ ${var#0} -gt 0 ]]
+        then
+                echo -e "\e[30;42m$1 has a Read Error Rate of ${var#0} \e[0m"
+        fi
+
 
         # SAS Read errors
 
@@ -97,6 +134,7 @@ function smartcheck()
         if [[ $var -gt 0 ]]
         then
                 echo -e "\e[0;41m$1 $var SAS Read Errors\e[0m"
+                sas=1
                 error=$(($error + 1))
         elif [ ${DEBUG} ] && [[ $var =~ ^[0-9]+$ ]]
         then
@@ -110,15 +148,30 @@ function smartcheck()
         if [[ $var -gt 0 ]]
         then
                 echo -e "\e[0;41m$1 $var SAS Write Errors\e[0m"
+                sas=1
                 error=$(($error + 1))
         elif [ ${DEBUG} ] && [[ $var =~ ^[0-9]+$ ]]
         then
                 echo -e "\e[30;42m$1 $var SAS Write Errors\e[0m"
         fi
 
+        # SAS Verify errors
+
+        #var=`smartctl -a $1 | egrep "verify:" | awk '{print $8}'`
+        var=`echo "$smartoutput"  | egrep "verify:" | awk '{print $8}'`
+        if [[ $var -gt 0 ]]
+        then
+                echo -e "\e[0;41m$1 $var SAS Verify Errors\e[0m"
+                sas=1
+                error=$(($error + 1))
+        elif [ ${DEBUG} ] && [[ $var =~ ^[0-9]+$ ]]
+        then
+                echo -e "\e[30;42m$1 $var SAS Verify Errors\e[0m"
+        fi
+
         # SAS post factory defects
 
-        var=`echo "$smartoutput"  | grep -i "grown defect" | sed 's/Elements in grown defect list: //'`
+        var=`echo "$smartoutput"  | grep -i "grown defect" | sed 's/Elements in grown defect list: //' | grep -iv "not available"`
         if [[ $var -gt 0 ]] && [ ${DEBUG} ]
         then
                 sleep 0
@@ -150,7 +203,7 @@ do
 done
 # Check disks attached to the board directly or in passthrough (BSD)
 #for i in `ls /dev/pass*|egrep '^(\/)dev(\/)pass[0-9]+$'`;
-for i in `find /dev -type c -name 'pass*' | egrep '^(\/)dev(\/)pass[0-9]$'`;
+for i in `find /dev -type c -name 'pass*' | egrep '^(\/)dev(\/)pass[0-9]+$'`;
 do
         smartcheck "$i" $DEBUG
         rval=$?
@@ -173,13 +226,38 @@ do
         agerror=$(($agerror + $rval))
 done
 
-# Check disks behind an lsi card - fixed at sda at the moment
+# Check scsi disks behind an lsi card - fixed at sda at the moment
 for i in `seq 0 20`
 do
         smartcheck "-d megaraid,$i /dev/sda -T permissive" $DEBUG
         rval=$?
         agerror=$(($agerror + $rval))
 done
+
+#Check disks behind an lsi card - fixed at sda at the moment
+for i in `seq 0 20`
+do
+        smartcheck "-d sat+megaraid,$i /dev/sda -T permissive" $DEBUG
+        rval=$?
+        agerror=$(($agerror + $rval))
+done
+
+# Check scsi disks behind an HPcard - fixed at sda at the moment
+for i in `seq 0 20`
+do
+        smartcheck "-d cciss,$i /dev/sda -T permissive 2> /dev/null" $DEBUG
+        rval=$?
+        agerror=$(($agerror + $rval))
+done
+
+
+if [[ ${DEBUG} &&  ${sas} ]]
+then
+        echo -e "---"
+        echo "NOTICE: SAS error counters can be reset using sg3_utils command"
+        echo "sg_logs -R /dev/device"
+        echo -e "---"
+fi
 
 if [[ $agerror -gt 0 ]]
 then
